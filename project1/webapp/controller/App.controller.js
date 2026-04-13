9e5de81aedc3039d7a83a6d5eb52f6184557f893
chandana.yeranagu@msg-global.com <chandana.yeranagu@msg-global.com>
2026-04-13 07:34:56 +0000

    Initial Commit

---
+++ project1/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("msg.ri.project1.controller.App", {
+        onInit() {
+        }
+    });
+});
